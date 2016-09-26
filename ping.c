6d0fb7383875b079690db96e98a7260dfdef8f47
Josué Alexander Baquiax Batén <alexbqb@galileo.edu.gt>
2016-09-25 22:41:22 -0600

    Advances.

--- ping.c
+++ ping.c
@@ -1,28 +1,58 @@
-#include <netinet/ip_icmp.h>
+#include <stdio.h>
 #include<sys/socket.h>
 
-unsigned short checksum(void *b, int len) {	
-    unsigned short *buf = b;
-	unsigned int sum=0;
-	unsigned short result;
+typdef struct {
+	uint8_t type;
+	uint8_t code;
+	uint16_t chksum;
+	uint32_t data;
+} icmp_header_struct;
 
-	for ( sum = 0; len > 1; len -= 2 )
-		sum += *buf++;
+int32_t checksum(uint16_t *buf, int32_t len) {    
+    int32_t nleft = len;
+    int32_t sum = 0;
+    uint16_t *w = buf;
+    uint16_t answer = 0;
 
-	if ( len == 1 )
-		sum += *(unsigned char*)buf;
+    while(nleft > 1) {
+        sum += *w++;
+        nleft -= 2;
+    }
+    
+    if(nleft == 1) {
+        sum += *(uint8_t *)w;
+    }
 
-	sum = (sum >> 16) + (sum & 0xFFFF);
-	sum += (sum >> 16);
-	result = ~sum;
-	return result;
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+    answer = ~sum;
+    return answer;
 }
 
-void ping(char *ipAddress) {	
-	int pingSocket =     
+void ping(char *ip_address) {	
+	int ping_socket = socket(PF_INET, SOCK_RAW, 1);
+	if ping_socket < 0 {
+		printf("El socket no se ha creado correctamente");
+		exit(0);
+	}     
+
+	icmp_header_struct header;
+	header.code = 8; //echo
+	header.type = 0;
+	header.chksum = 0;
+	header.data = 0;
+
+	header.chksum =  checksum((uint16_t *)&header, sizeof(header));
+
+	struct sockaddr_in ip_address;
+	ip_address.sin_family = AF_INET;
+	ip_address.sin_port = 0;
+	ip_address.sin_address.s_addr = inet_addr(ip_address);
+
+	int result = sendto(ping_socket, &header, sizeof(header), 0, (struct sockaddr*)&ip_address, sizeof(ip_address));
+
 }
 
-int main(int count, char *strings[]) {
-
+int main(int count, char *strings[]) {	
     ping("127.0.0.1");
 }
