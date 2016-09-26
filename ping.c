f6da7e2ff60e4edd548354a1a6eca98f0a0d2f69
baquiax <alexbqb@galileo.edu>
2016-09-25 23:13:31 -0600

    PING initialized!

--- ping.c
+++ ping.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-#include<sys/socket.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-typdef struct {
+typedef struct {
 	uint8_t type;
 	uint8_t code;
 	uint16_t chksum;
 	uint32_t data;
 } icmp_header_struct;
+
 
 int32_t checksum(uint16_t *buf, int32_t len) {    
     int32_t nleft = len;
@@ -31,28 +34,33 @@ int32_t checksum(uint16_t *buf, int32_t len) {
 
 void ping(char *ip_address) {	
 	int ping_socket = socket(PF_INET, SOCK_RAW, 1);
-	if ping_socket < 0 {
-		printf("El socket no se ha creado correctamente");
+	if (ping_socket <= 0) {
+		printf("El socket no se ha creado correctamente %d\n", ping_socket);
 		exit(0);
 	}     
 
 	icmp_header_struct header;
-	header.code = 8; //echo
+
+	header.code = 8;
 	header.type = 0;
 	header.chksum = 0;
 	header.data = 0;
 
 	header.chksum =  checksum((uint16_t *)&header, sizeof(header));
 
-	struct sockaddr_in ip_address;
-	ip_address.sin_family = AF_INET;
-	ip_address.sin_port = 0;
-	ip_address.sin_address.s_addr = inet_addr(ip_address);
+	struct sockaddr_in ip;
+	ip.sin_family = AF_INET;
+	ip.sin_port = 0;
+	ip.sin_addr.s_addr = inet_addr(ip_address);
 
-	int result = sendto(ping_socket, &header, sizeof(header), 0, (struct sockaddr*)&ip_address, sizeof(ip_address));
-
+	int result = sendto(ping_socket, &header, sizeof(header), 0, (struct sockaddr*)&ip, sizeof(ip));
+	printf("ICMP sent! Response: %d\n", ping_socket);
 }
 
-int main(int count, char *strings[]) {	
-    ping("127.0.0.1");
+int main(int argc, char *strings[]) {
+	if (argc < 2) {
+		printf("An IP is required\n");
+		exit(0);
+	}
+    ping(strings[1]);
 }
